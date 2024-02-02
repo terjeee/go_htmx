d7afe784cc8286c9861345a984e811d1c6209973
Terje O <terje.osevelle@sharebox.no>
2024-02-02 19:34:50 +0100

    mjau

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go_htmx
+
+go 1.21.6
--- test_01/main.go
+++ test_01/main.go
@@ -107,8 +107,97 @@ func main() {
 	// ARRAYS - fixed length, same type, indexable, contiguoous in memory 
 	// ARRAYS - fixed length, same type, indexable, contiguoous in memory
 	
-	var intArry [3]int32
-	fmt.Println(intArry)
+	var intArr [3]int32
+	fmt.Println(intArr[0])
+	fmt.Println(intArr[1:3])
+	intArr[1] = 22
+	fmt.Println(&intArr[0])
+	fmt.Println(&intArr[1])
+	fmt.Println(&intArr[2])
+
+	var intArray [4]int32 = [4]int32{1, 2, 3, 4}
+	fmt.Println(intArray)
+	intArray2 := [4]float32{3, 2, 1} 
+	fmt.Println(intArray2)
+	intArray3 := [...]float32{3, 2, 1} 
+	fmt.Println(intArray3)
+
+	// SLICES
+	// SLICES
+
+	var intSlice []float64 = []float64{7, 8, 9}
+	fmt.Printf("intSlice: %v\n", intSlice)
+	fmt.Printf("Length = %v\nCapacity is %v\n", len(intSlice), cap(intSlice))
+	intSlice = append(intSlice, 10)
+	fmt.Printf("intSlice: %v\n", intSlice)
+	fmt.Printf("Length = %v\nCapacity is %v\n", len(intSlice), cap(intSlice))
+	// fmt.Println(intSlice[4]) // runtime error: index out of range [4] with length 4
+
+	var intSlice2 []float64 = []float64{55, 56, 57}
+	fmt.Println(intSlice2)
+	intSlice2 = append(intSlice2, intSlice... )
+	fmt.Println(intSlice2)
+
+	// MAP
+	// MAP
+
+	var myMap map[string]uint8 = make(map[string]uint8)
+	fmt.Println(myMap)
+	
+	var myMap2 = map[string]uint8{"Jon" : 17, "Knut": 1}
+	fmt.Println(myMap2)
+	fmt.Println(myMap2["Jon"])
+	fmt.Println(myMap2["finnesIkke"])
+	var age, ok = myMap2["finnesIkke"]
+	if ok {
+		fmt.Println(age, ok)
+		} else {
+			fmt.Printf("age: %v - doesExist: %v\n", age, ok)
+	}	
+	
+	for name, age := range myMap2{
+		fmt.Printf("forLoopMap - name: %v, age: %v\n", name, age)
+	}
+
+	delete(myMap2, "Knut")
+	
+	for name, age := range myMap2{
+		fmt.Printf("forLoopMap(after delete()) - name: %v, age: %v\n", name, age)
+	}
+
+	for index, value := range intArr{
+		fmt.Printf("forLoopArr - index: %v, value: %v\n", index, value)
+	}
+
+	// WHILE LOOP
+	// WHILE LOOP
+
+	var i uint8 = 0
+	for i <= 5 {
+		fmt.Println(i)
+		i = i + 1
+	}
+
+	for i := 100; i <= 105; i++ {
+		fmt.Println(i)
+	}
+
+	// MATHS
+	// MATHS
+
+	number := 10
+	number--
+	fmt.Println(number)
+	number++
+	fmt.Println(number)
+	number += 10
+	fmt.Println(number)
+	number -= 10
+	fmt.Println(number)
+	number *= 10
+	fmt.Println(number)
+	number /= 10
+	fmt.Println(number)
 }
 
 // FUNCTIONS
